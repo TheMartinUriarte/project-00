f39e4b0fdd3320530857c9b2a90cd2f871436b62
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2016-11-05 20:01:21 -0700

    Added player1 image. Added function to display .Player1 and discovered keycodes for wanted keys.

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,1 +1,17 @@
-Starter Page
+$(document).ready(function() {
+
+	console.log("JS is working!");
+
+});
+
+$(function() {
+  var p1 = '<div class="Player1"></div>';
+  $(".road1").append(p1);
+
+  $(document).keydown(function(e) {
+
+    alert(e.keyCode);
+  });
+});
+
+//Keycodes for game - W: 87, A: 65, S: 83, D: 68, Up: 38, Left: 37, Down: 40, Right: 39.
