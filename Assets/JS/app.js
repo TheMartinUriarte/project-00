c2fe174944bdc53ca06623435fddf7104cd1a857
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2016-11-05 22:13:40 -0700

    Added funcationality to Player1 model. Car now advances up the screen by increments of 7px

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -8,10 +8,17 @@ $(function() {
   var p1 = '<div class="Player1"></div>';
   $(".road1").append(p1);
 
-  // $(document).keydown(function(e) {
-  //
-  //   alert(e.keyCode);
-  // });
+  $(document).keydown(function(e) {
+
+  var position = $(".Player1").position();
+
+  switch (e.keyCode)
+  {
+
+    case 87:
+      $(".Player1").css('top', position.top - 7 + 'px');
+  }
+});
 });
 
 //Keycodes for game - W: 87, A: 65, S: 83, D: 68, Up: 38, Left: 37, Down: 40, Right: 39.
