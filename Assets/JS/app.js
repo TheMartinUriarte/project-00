8bf8594b9d27c372b132e0fa093ca326a53287a8
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2016-11-05 20:18:56 -0700

    Properly alligned Player 1 car via CSS

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -8,10 +8,10 @@ $(function() {
   var p1 = '<div class="Player1"></div>';
   $(".road1").append(p1);
 
-  $(document).keydown(function(e) {
-
-    alert(e.keyCode);
-  });
+  // $(document).keydown(function(e) {
+  //
+  //   alert(e.keyCode);
+  // });
 });
 
 //Keycodes for game - W: 87, A: 65, S: 83, D: 68, Up: 38, Left: 37, Down: 40, Right: 39.
